dba302ecad12bf4bc0e4393cca72306d72eb96f7
stormytuna <stormytuna@outlook.com>
2024-02-12 12:11:02 +0000

    rename to DecorativePotions

--- Content/DecorativePotionItem.cs
+++ Content/DecorativePotionItem.cs
@@ -2,7 +2,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace PlaceablePotions.Content;
+namespace DecorativePotions.Content;
 
 public class DecorativePotionItem : ModItem
 {
---
+++ DecorativePotions.cs
@@ -0,0 +1,6 @@
+using Terraria.ModLoader;
+
+namespace DecorativePotions;
+
+public class DecorativePotions : Mod
+{ }
